85c402c0a80027eb3b825044a462c5293ad9d7a2
Grzesiek <dadmor@gmail.com>
2025-06-26 09:20:38 +0200

    0.0.26

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
@@ -13,11 +13,15 @@ export default defineConfig({
       brotliSize: true,
     }),
   ],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
   build: {
     rollupOptions: {
       output: {
